d7479c4ec5668d74277fd8c139ce4cb6c2bcede9
jashjchoi <jashjchoi@gmail.com>
2021-06-05 20:04:24 +0000

    It's finally working again phew

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 // imports npm packages
 const express = require('express');
 const app = express();
+let port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const DownTown = require('./models/place');
 
 const dbURI = 'mongodb+srv://tulsaMapsUser:thereare4ofus!@cluster0.91cna.mongodb.net/LocallyOwned?retryWrites=true&w=majority';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }).then((result) => {
-  // listen for requests
-  app.listen(5000);
-}).catch((err) => {
-  console.log('there is an error');
-});
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+// .then((result) => {
+//   // listen for requests
+//   app.listen(5000);
+// }).catch((err) => {
+//   console.log('there is an error');
+// });
 
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose is connected!!!!');
-});
+// mongoose.connection.on('connected', () => {
+//   console.log('Mongoose is connected!!!!');
+// });
+
 
 app.set('view engine', 'ejs');
 
@@ -36,6 +39,8 @@ app.get('/all-places', (req, res) => {
   DownTown.find({ })
     .then((result) => {
       res.send(result);
+      console.log("hello world");
+      console.log(result);
     })
     .catch((err) => {
       console.log(err);
@@ -46,9 +51,13 @@ app.get('/single-place', (req, res) => {
   DownTown.findById('60ba78f5650b7c69b4f57a17')
     .then((result) => {
       res.send(result);
-      console.log('Hello JS');
+      console.log('Search by ID works');
     })
     .catch((err) => {
       console.log(err);
     });
+});
+
+app.listen(port, () => {
+  console.info(`App is listening on port http://localhost:${port}`);
 });
