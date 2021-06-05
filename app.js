731076ddde94585e926be92ae75e5fc19ef36037
jashjchoi <jashjchoi@gmail.com>
2021-06-05 21:47:10 +0000

    Add keyword search functions

--- app.js
+++ app.js
@@ -7,17 +7,6 @@ const DownTown = require('./models/place');
 
 const dbURI = 'mongodb+srv://tulsaMapsUser:thereare4ofus!@cluster0.91cna.mongodb.net/LocallyOwned?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
-// .then((result) => {
-//   // listen for requests
-//   app.listen(5000);
-// }).catch((err) => {
-//   console.log('there is an error');
-// });
-
-// mongoose.connection.on('connected', () => {
-//   console.log('Mongoose is connected!!!!');
-// });
-
 
 app.set('view engine', 'ejs');
 
@@ -25,6 +14,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
+
 
 // Get and Response
 app.get('/', (req, res) => {
@@ -57,6 +47,95 @@ app.get('/single-place', (req, res) => {
       console.log(err);
     });
 });
+
+
+app.get('/show-cafes', (req, res) => {
+  DownTown.find({"googlePlaceInfo.types": "cafe"})
+    .then((result) => {
+      res.send(result);
+      console.log("Search by Keyword: CAFE working!!!");
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+
+app.get('/show-bars', (req, res) => {
+  DownTown.find({"googlePlaceInfo.types": "bar"})
+    .then((result) => {
+      res.send(result);
+      console.log("Search by Keyword: BARS working!!!");
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/show-food', (req, res) => {
+  DownTown.find({"googlePlaceInfo.types": "food"})
+    .then((result) => {
+      res.send(result);
+      console.log("Search by Keyword: FOOD working!!!");
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/show-stores', (req, res) => {
+  DownTown.find({"googlePlaceInfo.types": "store"})
+    .then((result) => {
+      res.send(result);
+      console.log("Search by Keyword: STORE working!!!");
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/show-gym', (req, res) => {
+  DownTown.find({"googlePlaceInfo.types": "gym"})
+    .then((result) => {
+      res.send(result);
+      console.log("Search by Keyword: GYM working!!!");
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// DownTown.find({"googlePlaceInfo.types": "bar"}, (err, data) => {
+//   if(err){
+//     console.log(err)
+//   } else {
+//     console.log(data)
+//   }
+// })
+
+// DownTown.find({"googlePlaceInfo.types": "food"}, (err, data) => {
+//   if(err){
+//     console.log(err)
+//     console.log('Search by FOOD works');
+//   } else {
+//     console.log(data)
+//   }
+// })
+
+// DownTown.find({"googlePlaceInfo.types": "store"}, (err, data) => {
+//   if(err){
+//     console.log(err)
+//     console.log('Search by STORE works');
+
+//   } else {
+//     console.log(data)
+//   }
+// })
 
 app.listen(port, () => {
   console.info(`App is listening on port http://localhost:${port}`);
